bab594133eb7ecb7968dc9ed7e89acb1a027fe24
Olmos Soto Manuel Alejandro <MSOLMOS@entel.cl>
2018-06-12 16:36:32 -0400

    Ejercicio3 done

--- Ejercicio3.rb
+++ Ejercicio3.rb
@@ -2,20 +2,27 @@
 h = {"x": 1, "y":2}
 
 #1 Agregar el string z con el valor 3
-
+h.store("z", 3)
+puts h
 
 #2 Cambiar el valor de x por 5
-
+h['x'] = 5
+puts h
 
 #3 Eliminar la clave y
-
+h.delete(:y)
+puts h
 
 #4 Si el hash tiene una clave llamada z mostrar en pantalla "yeeah"
-
+if h.has_key?(:z)
+    puts "yeeah"
+else
+    puts "no yeeah"
+end
 
 #5 Invertir el diccionario de forma que los valores sean las llaves y las llaves los valores
 # Ejemplo:
 # x = {"a": "hola" }
 # Se transforme en:
 # x = {"hola": "a"}
-
+h.invert

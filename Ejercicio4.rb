9579a58b3c541bc96ccf391e844907266d5c5980
Alejandro Olmos <manuelalejandro.soto1@gmail.com>
2018-06-12 21:20:45 -0400

    Resolved Exercises 1 to 6, just left the 7th

--- Ejercicio4.rb
+++ Ejercicio4.rb
@@ -7,9 +7,12 @@ personas = ["Carolina", "Alejandro", "Maria Jesús", "Valentín"]
 edades = [32, 28, 41, 19]
 
 #1 Se pide generar un hash con la información: personas_hash = {"Carolina": 32, "Alejandro":28, "María Jesús":41, "Valentín":19}
-puts Hash[personas.zip(edades)]
+merge_hash = Hash[personas.zip(edades)]
+puts merge_hash
 
 # 2 Crear un método que reciba el hash y devuelva la edad del hash pasado como argumento
 def hashBirth(hash)
-    
+    puts hash.values
 end
+
+hashBirth(merge_hash)
--- Ejercicio6.rb
+++ Ejercicio6.rb
@@ -4,24 +4,32 @@ es el precio de este.
 =end
 restaurant_menu = { "Ramen" => 3, "Dal Makhani" => 4, "Coffee" => 2 }
 
-
 # 1 Obtener el plato mas caro
-
+puts restaurant_menu.key(4)
 
 # 2 Obtener el plato mas barato
-
+puts restaurant_menu.key(2)
 
 # 3 Sacar el promedio del valor de los platos
-
+arr = restaurant_menu.each_value.to_a
+puts arr.sum / arr.length
 
 # 4 Crear un arreglo con solo los nombres de los platos
-
+arr_names = restaurant_menu.each_key.to_a
+puts arr_names
 
 # 5 Crear un arreglo con solo los valores de los platos
-
+arr_values = restaurant_menu.each_value.to_a
+puts arr_values
 
 # 6 Modificar el hash y agregar el IVA a los valores de los platos (multiplicar por 1.19)
-
+restaurant_menu.each_value do |value|
+    value*=1.19
+    puts value
+end
 
 # 7 Dar descuento del 20% para los platos que tengan un nombre de más 1 una palabra
-
+restaurant_menu.each do |key, value|
+    value-=(value*0.2) if key.include? " "
+    puts value
+end
